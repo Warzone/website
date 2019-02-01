a417894262e3eb9fff267b95ccfb5d651741da23
jellz <danielgulic@gmail.com>
2019-02-01 20:30:20 +1100

    export api url

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './grid.css';
 import App from './App';
+import conf from './config.json';
+
+export const config = conf;
+console.log('Using API ' + config.API_BASE);
 
 ReactDOM.render(<App />, document.getElementById('root'));
