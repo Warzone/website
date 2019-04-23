5449f5292c41e1938c834b544ee0f6b606829694
jellz <danielgulic@gmail.com>
2019-04-23 21:44:14 +1000

    register service worker

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './styles/simple-grid.css';
 import './global.css';
 import App from './App';
 import conf from './config.json';
+import * as serviceWorker from './serviceWorker';
 
 export const config = conf;
 console.log('Using API ' + config.API_BASE);
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.register();
