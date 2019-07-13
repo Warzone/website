40d947c0a9fa984df59dbb25d4e30002cee317fd
Daniel Gulic <danielgulic@gmail.com>
2019-07-14 05:12:06 +1000

    player page! closes #52

--- src/pages/PlayerInfo.js
+++ src/pages/PlayerInfo.js
@@ -2,22 +2,30 @@ import React, { Component } from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { config } from '../';
 import '../styles/pages/player-info.css';
+import PlayerStatsRow from '../components/player/PlayerStatsRow';
+import KillHistory from '../components/recent/KillHistory';
+import MatchHistory from '../components/recent/MatchHistory';
 
 class PlayerInfo extends Component {
   state = {
-    player: null
+    player: null,
+    recentMatches: []
   };
 
   async componentDidMount() {
+    let playerName = this.props.match.params.pid;
     let playerJson, playerRes;
-    playerRes = await fetch(
-      `${config.API_BASE}/mc/player/${this.props.match.params.pid}`
-    );
+    playerRes = await fetch(`${config.API_BASE}/mc/player/${playerName}`);
     if (!playerRes.ok)
       return console.error('Error fetching player: ' + playerRes.status);
     playerJson = await playerRes.json();
     this.setState({ player: playerJson });
     console.log(playerJson);
+
+    let matchRes, matchJson;
+    matchRes = await fetch(`${config.API_BASE}/mc/match/latest/${playerName}`);
+    matchJson = await matchRes.json();
+    this.setState({ recentMatches: matchJson });
   }
 
   render() {
@@ -32,68 +40,19 @@ class PlayerInfo extends Component {
           )}
           {player && (
             <div>
-              <div className='player-info-stats row'>
-                <div className='col-4 player-info-col'>
-                  <h1 className='white player-name justify vertical-center'>
-                    {player.user.name}
-                  </h1>
+              <PlayerStatsRow player={player} />
+              <div class='row'>
+                <div class='col-6'>
+									<div>Recent Kills</div>
+                  <KillHistory
+                    kills={player.deaths.sort((a, b) => a.date - b.date).reverse()}
+                  />
                 </div>
-                <div className='col-8 player-info-col'>
-                  <div className='player-info-row row'>
-                    <div className='col-2 center'>
-                      <h1 className='no-padding-margin'>{player.user.kills}</h1>
-                      <span style={{ fontSize: '15px', textAlign: 'center' }}>
-                        Kills
-                      </span>
-                    </div>
-                    <div className='col-2 center'>
-                      <h1 className='no-padding-margin'>
-                        {player.user.deaths}
-                      </h1>
-                      <span style={{ fontSize: '15px', textAlign: 'center' }}>
-                        Deaths
-                      </span>
-                    </div>
-                    <div className='col-2 center'>
-                      <h1 className='no-padding-margin'>
-                        {Math.round(
-                          (player.user.kills / player.user.deaths) * 100
-                        ) / 100}
-                      </h1>
-                      <span style={{ fontSize: '15px', textAlign: 'center' }}>
-                        K/D
-                      </span>
-                    </div>
-                    <div className='col-2 center'>
-                      <h1 className='no-padding-margin'>
-                        {player.user.wins}
-                      </h1>
-                      <span style={{ fontSize: '15px', textAlign: 'center' }}>
-                        Wins
-                      </span>
-                    </div>
-                    <div className='col-2 center'>
-                      <h1 className='no-padding-margin'>
-                        {player.user.losses}
-                      </h1>
-                      <span style={{ fontSize: '15px', textAlign: 'center' }}>
-                        Losses
-                      </span>
-                    </div>
-                    <div className='col-2 center'>
-                      <h1 className='no-padding-margin'>
-                        {Math.round(
-                          (player.user.wins / player.user.losses) * 100
-                        ) / 100}
-                      </h1>
-                      <span style={{ fontSize: '15px', textAlign: 'center' }}>
-                        W/L
-                      </span>
-                    </div>
-                  </div>
-                </div>
+                <div class='col-6'>
+									<div>Recent Matches</div>
+									<MatchHistory matches={this.state.recentMatches} />
+								</div>
               </div>
-              <div>player info cards</div>
             </div>
           )}
         </div>
