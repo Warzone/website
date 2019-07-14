033baa1238c39f424b42be2b9c9fe92f7c69e5c1
Daniel Gulic <danielgulic@gmail.com>
2019-07-14 17:39:58 +1000

    convert feature grid images to WebP

--- src/components/FeatureGrid.js
+++ src/components/FeatureGrid.js
@@ -8,17 +8,17 @@ class FeatureGrid extends Component {
         <div className='row'>
           <div className='col-4 center'>
             <h3>Automated Matches</h3>
-            <img src='https://i.imgur.com/DnobRWZ.png' alt='A Warzone KOTH Map' className='feature-grid-image' />
+            <img src='/img/fg-open.webp' alt='A Warzone KOTH Map' className='feature-grid-image' />
             <p>The server is always open for you to come on and practice your skills with friends!</p>
           </div>
           <div className='col-4 center'>
             <h3>Match Details</h3>
-            <img src='https://i.imgur.com/0N5yGOV.png' alt='Warzone Match Details Page' className='feature-grid-image' />
+            <img src='/img/fg-match.webp' alt='Warzone Match Details Page' className='feature-grid-image' />
             <p>Relive past matches with our match tracking</p>
           </div>
           <div className='col-4 center'>
             <h3>Player Stats</h3>
-            <img src='https://i.imgur.com/ozg7teT.png' alt='Warzone Player Stats Page' className='feature-grid-image' />
+            <img src='/img/fg-player.webp' alt='Warzone Player Stats Page' className='feature-grid-image' />
             <p>Comprehensive player stats include K/D and W/L</p>
           </div>
         </div>
