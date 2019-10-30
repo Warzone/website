de190d15ff5f314ec37f5a0221690369f13c8f7f
Indicado <xu3char@gmail.com>
2019-10-30 23:38:50 +0100

    Add staff page

--- src/Router.js
+++ src/Router.js
@@ -8,6 +8,7 @@ import ServerRules from './pages/ServerRules';
 import NotFound from './pages/NotFound';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import Play from './pages/Play';
+import Staff from './pages/Staff';
 import ScrollToTopRoute from './components/ScrollToTopRoute';
 import MatchInfo from './pages/MatchInfo';
 import PlayerInfo from './pages/PlayerInfo';
@@ -29,6 +30,7 @@ class Router extends Component {
 						<ScrollToTopRoute exact path='/rules' component={ServerRules} />
 						<ScrollToTopRoute exact path='/privacy' component={PrivacyPolicy} />
 						<ScrollToTopRoute exact path='/play' component={Play} />
+						<ScrollToTopRoute exact path='/staff' component={Staff} />
 						<ScrollToTopRoute exact path='/m/:mid' component={MatchInfo} />
 						<ScrollToTopRoute exact path='/p/:pid' component={PlayerInfo} />
 						<ScrollToTopRoute component={NotFound} />
---
+++ src/pages/Staff.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Tooltip from '@material-ui/core/Tooltip';
+import { Link } from 'react-router-dom';
+import NotFound from './NotFound';
+import { config } from '../';
+
+export default function Staff() {
+	const [staff, setStaff] = useState([]);
+	const [notFound, setNotFound] = useState(false);
+
+	useEffect(() => {
+		async function fetchData() {
+			let ranksJson, ranksRes;
+			ranksRes = await fetch(`${config.API_BASE}/mc/ranks`);
+			if (!ranksRes.ok) {
+				return console.error('Error fetching ranks');
+			}
+
+			ranksJson = await ranksRes.json();
+			if (ranksJson.notFound) return setNotFound(true);
+
+			ranksJson = ranksJson.filter(function(rank) {
+				return rank.staff !== false;
+			});
+
+			ranksJson.reverse().map(async (rank, i) => {
+				rank.name = rank.name.charAt(0).toUpperCase() + rank.name.slice(1);
+
+				rank.players = [];
+				rank.players = await (await (await fetch(
+					`${config.API_BASE}/mc/rank/${rank.name}/players`
+				)).json()).users;
+
+				if (rank.players[i] !== []) {
+					rank.playersLoaded = true;
+				}
+
+				if (ranksJson.every((e) => e.hasOwnProperty('playersLoaded'))) {
+					setStaff(ranksJson);
+					console.log(ranksJson);
+				}
+			});
+		}
+
+		fetchData();
+	}, []);
+
+	return (
+		<div className='page'>
+			<div className='container'>
+				<div>
+					{notFound && <NotFound />}
+					{staff.length === 0 && !notFound && (
+						<div className='center'>
+							<CircularProgress />
+						</div>
+					)}
+					{staff.length > 0 && (
+						<div>
+							<h1 style={{ marginBottom: '0px' }}>
+								Staff{' '}
+								<small style={{ fontSize: '17px' }}>
+									People who help keep the server running
+								</small>
+							</h1>
+							<hr />
+						</div>
+					)}
+
+					{staff.map((rank) => (
+						<div class='row'>
+							<h2>
+								{rank.name}{' '}
+								<small style={{ fontSize: '13px', color: 'white' }}>
+									{rank.players.length}
+								</small>
+							</h2>
+							{rank.players.map((player) => (
+								<div class='col-md-3 col-sm-6'>
+									<Link to={`/p/${player.name}`}>
+										<Tooltip
+											disableFocusListener
+											disableTouchListener
+											placement='top'
+											title={player.name}
+										>
+											<img
+												class='img-rounded'
+												src={`https://crafatar.com/avatars/${player.uuid}?helm&amp;size=160`}
+												style={{ width: '120px', borderRadius: '10px' }}
+												alt='Staff head'
+											/>
+										</Tooltip>
+									</Link>
+								</div>
+							))}
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
