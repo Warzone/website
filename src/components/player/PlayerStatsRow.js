4117a332582833822d66bc527f60d00bacb932ec
Daniel Gulic <danielgulic@gmail.com>
2019-12-12 17:48:52 +1100

    add level to player stats, remove k/d and w/l

--- src/components/player/PlayerStatsRow.js
+++ src/components/player/PlayerStatsRow.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class PlayerStatsRow extends Component {
+	getLevelColour(level) {
+			if (level <= 10) return '#AAAAAA';
+			else if (level <= 20) return '#00AAAA';
+			else if (level <= 30) return '#5555FF';
+			else if (level <= 40) return '#FF55FF';
+			else if (level <= 60) return '#AA00AA';
+			else if (level <= 80) return '#AA0000';
+			else if (level <= 100) return '#FF5555';
+			else if (level <= 120) return '#FFAA00';
+			else if (level <= 140) return '#FFFF55';
+			else if (level <= 160) return '#55FF55';
+			else return '#00AA00'; // levels above 160
+		}
+
 	render() {
 		let { player } = this.props;
 		return (
@@ -8,9 +22,7 @@ class PlayerStatsRow extends Component {
 				<div className='col-4 player-info-col'>
 					<img
 						style={{ width: '40px', marginBottom: '-75px' }}
-						src={`https://crafatar.com/renders/body/${
-							player.user.uuid
-						}?overlay`}
+						src={`https://crafatar.com/renders/body/${player.user.uuid}?overlay`}
 						alt='Player skin'
 					/>
 					<h1
@@ -23,6 +35,20 @@ class PlayerStatsRow extends Component {
 				<div className='col-8 player-info-col'>
 					<div className='player-info-row row'>
 						<div className='col-2 center'>
+							<h1
+								className='no-padding-margin'
+								style={{ color: this.getLevelColour(player.user.level) }}
+							>
+								{player.user.level}
+							</h1>
+							<span
+								className='player-info-stat-title'
+								style={{ color: this.getLevelColour(player.user.level) }}
+							>
+								Level
+							</span>
+						</div>
+						<div className='col-2 center'>
 							<h1 className='no-padding-margin'>{player.user.kills}</h1>
 							<span className='player-info-stat-title'>Kills</span>
 						</div>
@@ -30,10 +56,10 @@ class PlayerStatsRow extends Component {
 							<h1 className='no-padding-margin'>{player.user.deaths}</h1>
 							<span className='player-info-stat-title'>Deaths</span>
 						</div>
-						<div className='col-2 center'>
+						{/* <div className='col-1.7 center'>
 							<h1 className='no-padding-margin'>{player.user.kdr}</h1>
 							<span className='player-info-stat-title'>K/D</span>
-						</div>
+						</div> */}
 						<div className='col-2 center'>
 							<h1 className='no-padding-margin'>{player.user.wins}</h1>
 							<span className='player-info-stat-title'>Wins</span>
@@ -42,10 +68,10 @@ class PlayerStatsRow extends Component {
 							<h1 className='no-padding-margin'>{player.user.losses}</h1>
 							<span className='player-info-stat-title'>Losses</span>
 						</div>
-						<div className='col-2 center'>
+						{/* <div className='col-1.7 center'>
 							<h1 className='no-padding-margin'>{player.user.wlr}</h1>
 							<span className='player-info-stat-title'>W/L</span>
-						</div>
+						</div> */}
 					</div>
 				</div>
 			</div>
