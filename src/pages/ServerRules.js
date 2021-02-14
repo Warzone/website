f78c250639abf617ebecc45fd5e82dba2c33091f
Daniel <danielgulic@gmail.com>
2021-02-14 22:55:29 +1100

    final changes before deploy

--- src/pages/ServerRules.js
+++ src/pages/ServerRules.js
@@ -8,7 +8,7 @@ class ServerRules extends Component {
 					<div>
 						<h1>Server Rules</h1>
 						<span className='bold'>
-							This page was last updated on January 30th, 2021.
+							This page was last updated on February 14th, 2021 (revamped).
 						</span>
 						<p>
 							Failure to abide by any of the rules listed on this page may
@@ -22,44 +22,37 @@ class ServerRules extends Component {
 						<h2>General Composure</h2>
 						<ol>
 							<li>
-								Ensure that you are respectful to all players and are helping to
-								maintain Warzone's calm atmosphere while on our platforms.
+								Ensure that you are respectful to all players and are
+								maintaining a calm and positive atmosphere while on our server.
 								Harassment and bullying are not allowed.
 							</li>
 							<li>
-								Toxic behavior on any of our platforms is not allowed. Toxic
-								behavior can be considered as showing bad sportsmanship,
-								disrespecting others, and acting in a demeaning way towards
-								others. We aim to provide all users with a safe and relaxed
-								environment; Warzone has no room for toxicity.
+								Toxic behavior such as acting negatively towards others, being a
+								bad sportsman, and disrespecting others is not allowed. We aim
+								to provide all players with a safe and relaxed environment;
+								Toxicity has no place on Warzone.
 							</li>
 							<li>
-								Revealing personal information is not allowed. This includes,
-								but is not limited to, IP addresses, home addresses and phone
-								numbers. Doing so may result in immediate removal from all
-								Warzone platforms.
+								Leaking players' personal information is not allowed. This
+								includes, but is not limited to, IP addresses, home addresses
+								and phone numbers.
 							</li>
 							<li>
-								Threatening other players or the server is not allowed. This
-								includes (D)DoS Threats, Dox threats, Death threats or other
-								threats of that nature.
+								Be aware that others may find excessive profanity repulsive and
+								disrespectful. While we're lenient when it comes to swearing, we
+								ask that you do not direct profanity towards other players.
 							</li>
 							<li>
-								Keep in mind that some users can be offended by profanity, and
-								feel unwelcome. While we're lenient when it comes to swearing,
-								we ask that you do not direct profanity towards other community
-								members.
+								Advertising and self-promoting are not allowed. This includes
+								promoting Minehut servers, external Minecraft servers, YouTube
+								channels, and Discord servers.
 							</li>
 							<li>
-								Advertisement and self promotion is strictly prohibited. This
-								includes Minecraft or Discord servers, and any form of social
-								media.
-							</li>
-							<li>
-								Utilizing our platforms as a marketplace will not be tolerated.
-								Purchasing, selling, or soliciting anything through our services
-								is not allowed (only if you are not purchasing from our official
-								store). Keep in mind that this is not a market.
+								Keep in mind that this is not a market. In order to protect our
+								players from scams and other financial offenses, selling,
+								soliciting, and/or buying anything through our services (that is
+								not from our official store) are not allowed. By extension,
+								scamming or defrauding other players is not allowed.
 							</li>
 							<li>
 								Discrimination of any kind is not allowed. This includes
@@ -67,31 +60,30 @@ class ServerRules extends Component {
 								sexual orientation, and other protected groups.
 							</li>
 							<li>
-								Using excessive capitalization in chat, flooding chat, inciting
-								spam or engaging in spam, is not allowed.
+								Using excessive capitalization in chat, flooding the chat and
+								spamming the chat are all not allowed.
 							</li>
 							<li>
 								Evading punishments is not allowed. This includes using
-								alternate accounts to join our Discord or Minecraft server to
-								evade a punishment. Users who are muted in-game are not
-								permitted to chat in our{' '}
-								<span className='white'>#server-chat</span> Discord bridging
+								alternate accounts to join our Discord or Minecraft server when
+								banned. Users who are muted in-game are not permitted to chat in
+								our <span className='white'>#server-chat</span> Discord bridging
 								channel.
 							</li>
 							<li>
-								Encouraging other players to break rules is not allowed. If a
-								player breaks a rule following your directions, you may also be
-								responsible.
-							</li>
-							<li>
 								We ask that you are respectful toward members of the staff team,
-								Make sure to treat our staff members as you would with any other
-								player. We ask that you listen to as you would be to a normal
-								player. Make sure to follow instructions from staff.
+								as you would be to normal players. Make sure to follow
+								instructions from staff.
 							</li>
+							<li>Encouraging other players to break rules is not allowed.</li>
 							<li>
 								Impersonating staff members on any platform by rank,
 								association, or name is not allowed.
+							</li>
+							<li>
+								Threatening other players or the server is not allowed. This
+								includes (D)DoS Threats, Dox threats, Death threats and other
+								threats of that nature.
 							</li>
 						</ol>
 					</div>
@@ -100,34 +92,32 @@ class ServerRules extends Component {
 						<ol>
 							<li>You must have an appropriate username and skin.</li>
 							<li>
-								Do not try to harm your team. This includes game throwing
-								(intentionally losing a game), cross teaming (teaming with
-								opponents), and team griefing (breaking blocks under teammates,
-								blocking spawn, stealing from chests, destroying your team's
-								side and preventing your team from achieving objectives, etc.).
+								Do not attempt to harm your team. This includes game throwing
+								(intentionally losing a game) and team griefing (breaking blocks
+								under teammates, stealing from chests, sabotaging your team's
+								defences, preventing your team from achieving objectives, etc.).
 							</li>
 							<li>
-								Map/statistic abuse such as grinding alt accounts, spawn
-								killing, leaving the game just before being killed, switching
-								teams just before you lose, and anything similar that may
-								positively affect your stats above other players on the server
-								are not allowed.
+								Map and statistic abuse are not allowed. This includes grinding
+								alt accounts, spawn killing, leaving the game just before being
+								killed, switching teams just before you lose, and anything
+								similar that may positively affect your stats above other
+								players on the server are not allowed.
 							</li>
 							<li>
-								All client modifications (including unofficial clients like
-								Badlion Client and Lunar Client) are generally considered "use
-								at your own risk". Client modifications that give you an unfair
-								advantage over other players are not allowed. Mods that purely
-								affect your client visually are allowed (e.g. OptiFine, 5zig,
-								LabyMod).
+								Client modifications that give you an unfair advantage over
+								other players are not allowed. Unofficial clients such as
+								Badlion Client and Lunar Client are considered "use at your own
+								risk." Purely aesthetic mods such as OptiFine, 5zig, and LabyMod
+								are allowed.
 							</li>
 							<li>
 								Macros, autoclickers, multiple attack functions, physical
 								hardware or software modifications to allow automated clicking,
-								changing standard mouse settings, modifications to standard
-								hardware limitations, etc. are not allowed. Any clicks beyond
-								one click at a time is not allowed; butterfly clicking and drag
-								clicking are both unacceptable clicking methods.
+								modifications of standard hardware limitations, etc. are not
+								allowed. Any number of clicks beyond one click at a time is not
+								allowed, which means butterfly clicking and drag clicking are
+								both not allowed.
 							</li>
 						</ol>
 					</div>
@@ -153,13 +143,10 @@ class ServerRules extends Component {
 						<h2>Forums</h2>
 						<ol>
 							<li>
-								If you are not the creator of a report or an appeal, you may not
-								post replies unless you are a staff member.
+								Participating in reports or appeals threads that you are not
+								directly involved is not allowed.
 							</li>
-							<li>
-								Both the Reports & Appeals forum categories are strictly for
-								reports & appeals. You may not post anything unrelated.
-							</li>
+							<li>All posts should be on topic.</li>
 						</ol>
 					</div>
 					<hr />
